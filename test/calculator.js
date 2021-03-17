34c2ead290910f6eb7b656df08a316091145cb84
LangWarkentin <christianwarkentin@googlemail.com>
2021-03-17 17:14:13 +0100

    Änderung "Warten" auf 2sec

--- test/calculator.js
+++ test/calculator.js
@@ -22,7 +22,7 @@ describe('demo calculator tests', function () {
         // expect(result.getText()).toEqual('5');
         homepage.verifyResult('7');
 
-        browser.sleep(2000)
+        browser.sleep(3000)
     });
 
     it('addition test', function () {
